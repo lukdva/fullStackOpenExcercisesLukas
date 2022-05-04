2d75f60bcef1362835ef25f25f7fb6e6214db717
LukasD <lukas.dvarionis@gmail.com>
2022-05-04 18:31:12 +0300

    1.2: course information step2 :
    Created Part component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -21,17 +21,13 @@ const Total = (props) => <p>Number of exercises {arraySum(props.exercisesArray) 
 const Content = (props) => {
 return (
   <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
   </>
 )}
+const Part = (props) => <p> {props.part} {props.exercises} </p>
+
 
 const arraySum = (array) => {
   let sum = 0;
